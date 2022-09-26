fcf70957beda5d273291ea979e5abeee20388aa0
sofoyan gevork <aab81@bk.ru>
2022-09-26 14:52:54 +0300

    connection

---
+++ AdoHT1/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoHT1
+{
+    /// <summary>
+    /// Модель хранит данные Пользолвателя
+    /// </summary>
+    internal class Customer : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id
+        {
+            get;
+
+            set;
+
+        }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        private string _name = null!;
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        private string _phoneNumber = null!;
+       
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(_phoneNumber));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
--- AdoHT1/MainWindow.xaml.cs
+++ AdoHT1/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using SqlConnection = System.Data.SqlClient.SqlConnection;
 
 namespace AdoHT1
 {
@@ -20,17 +26,36 @@ namespace AdoHT1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection sqlConnection;
+        
         public MainWindow()
         {
             InitializeComponent();
+            Connection();
         }
 
+        private void Connection()
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["AdoDB"].ConnectionString);
+            sqlConnection.Open();
+
+            if(sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение открыто!");
+            }
+        }
+        
         private void button1_Copy_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
 
         }
