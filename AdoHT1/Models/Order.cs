8d7f8055cd137526f306d7c00fcd0ffedc0e2af2
sofoyan gevork <aab81@bk.ru>
2022-10-03 17:07:33 +0300

    Order

--- AdoHT1/Models/Order.cs
+++ AdoHT1/Models/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,23 +16,90 @@ namespace AdoHT1.Models
         /// <summary>
         /// Идентификатор
         /// </summary>
+        public int _id;
+
+        public double _summ;
+
+        public string _date;
+
+        public int _customerId;
+
+
+        /*****************************************************************************************************************************************
+         *****************************************************************************************************************************************/
+
         public int Id
         {
-            get;
-
-            set;
-
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
         }
 
-        public decimal Summ;
+        public double Summ
+        {
+            get => _summ;
+            set
+            {
+                _summ=value;
+                OnPropertyChanged(nameof(Summ));
+            }
+        }
 
-        public DateTime date;
+        public string Date
+        {
+            get => _date;
+            set
+            {
+                _date=value;
+                OnPropertyChanged(nameof(_date));
+            }
+        }
 
-        public int CustomerId;
+        public int CustomerId
+        {
+            get => _customerId;
+            set
+            {
+                _customerId=value;
+                OnPropertyChanged(nameof(CustomerId));
+            }
+        }
 
 
-        /*-------------------------------------------------------------------------------------------------------------------------------------------------------------
-        --------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        /*****************************************************************************************************************************************
+         *****************************************************************************************************************************************/
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Order()
+        {
+            Id = int.MaxValue;
+            CustomerId = int.MaxValue;
+            Date = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            Summ = double.MaxValue;
+        }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="customerId"></param>
+        /// <param name="summ"></param>
+        /// <param name="dateString"></param>
+        public Order(int id, int customerId, string dateString, double summ )
+        {
+            Id = id;
+            CustomerId = customerId;
+            Date = dateString;
+            Summ = summ;
+            
+        }
+
+        /*****************************************************************************************************************************************
+        *****************************************************************************************************************************************/
+
         /// <summary>
         /// Делегат-обработчик события PropertyChanged
         /// </summary>
